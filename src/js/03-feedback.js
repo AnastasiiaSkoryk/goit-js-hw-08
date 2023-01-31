96b449104d3c467241887a741b99a0936ac56bdb
AnastasiiaSkorik <skorik.anastasiia.97@gmail.com>
2023-01-31 22:22:21 +0200

    add js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+const KEY = 'feedback-form-state';
+const input = {};
+const inputForm = document.querySelector('.feedback-form');
+ 
+inputForm.addEventListener('input', throttle(newInput, 500));   
+    
+function newInput(event) {
+    input.email = inputForm.elements.email.value;
+    input.message = inputForm.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(input));
+  
+}
+ 
+updatePage();
+
+function updatePage() {
+    
+    const savedData = localStorage.getItem(KEY);
+
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        inputForm.email.value = email;
+        inputForm.message.value = message;
+    }
+}
+
+inputForm.addEventListener('submit', onFormSubmit);  
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const message = event.target.elements.message.value;
+  if (email === '' || message === '') {
+    alert('Будь ласка, заповніть усі поля!');
+  } else {
+    localStorage.removeItem('feedback-form-state');
+    console.log({ email, message });
+  }
+    
+    event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+}
